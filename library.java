175e3c4a3cb2db3c2433cbe7641cb46b391f8283
Zaara Mulani <your-github-zaaramulani09@gmail.com>
2026-07-14 19:29:20 +0530

    Implement add book feature using HashMap

--- library.java
+++ library.java
@@ -1,3 +1,17 @@
-public class Library{
-    
+import java.util.HashMap;
+
+
+public class Library {
+    private HashMap<Integer, Book> books;
+
+    public Library() {
+        this.books = new HashMap<>();
+    }
+
+    public void addBook(int bookId, String title, String author) {
+        Book newBook = new Book(bookId, title, author);
+        books.put(bookId, newBook);
+        System.out.println("Book added successfully");
+        System.out.println(newBook);
+    }
 }
