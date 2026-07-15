a9664aaa06a91674a54fdc14205569a51d9450d2
Zaara Mulani <your-github-zaaramulani09@gmail.com>
2026-07-16 00:28:27 +0530

    Added file persistence with save and load features

--- library.java
+++ library.java
@@ -1,4 +1,11 @@
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Library {
@@ -6,11 +13,13 @@ public class Library {
 
     public Library() {
         this.books = new HashMap<>();
+        loadBooks();
     }
 
     public void addBook(int bookId, String title, String author) {
         Book newBook = new Book(bookId, title, author);
         books.put(bookId, newBook);
+        saveBooks();
         System.out.println("Book added successfully");
         System.out.println(newBook);
     }
@@ -72,6 +81,44 @@ public class Library {
         else{
           System.out.println("Book with ID " + bookId + " not found.");
          }
+        }
+
+        public void saveBooks(){
+            try{
+                FileWriter writer = new FileWriter("books.txt");
+                for(Book book : books.values()) {
+                 writer.write(
+                book.getBookId() + "," +
+                book.getTitle() + "," +
+                book.getAuthor() + "," +
+                book.getIsBorrowed() + "\n");
+        }
+             writer.close();
+            }
+            catch(IOException e){
+              System.out.println("Error saving books: " + e.getMessage());
+            }
+        }
+        
+        public void loadBooks(){
+          try {
+         BufferedReader reader = new BufferedReader(new FileReader("books.txt"));
+         String line;
+        while((line = reader.readLine()) != null) {
+        String[] data = line.split(",");
+        int bookId = Integer.parseInt(data[0]);
+        String title = data[1];
+        String author = data[2];
+        boolean isBorrowed = Boolean.parseBoolean(data[3]);
+        Book book = new Book(bookId, title, author);
+        book.setIsBorrowed(isBorrowed);
+        books.put(bookId, book);
+}
+       reader.close();
+       }
+       catch(IOException e) {
+       System.out.println("Error loading books: " + e.getMessage());
+    }
         }
 }
 
